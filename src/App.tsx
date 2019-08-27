e9afb1802057c96f9fc5ec34d2dfd6ac1704f490
Ivan Stanojevic <ivan@byteout.com>
2019-08-27 16:45:49 +0200

    starter pack

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,7 @@ import "animate.css/animate.min.css";
 import {HashRouter as Router, Route} from "react-router-dom";
 import {Header} from "./components/header";
 import {Footer} from "./components/footer";
-import {About} from "./pages/about";
 import {Home} from "./pages/home";
-import {Projects} from "./pages/projects";
-import {Team} from "./pages/team";
-import {Contact} from "./pages/contact";
-import {Partners} from "./pages/partners";
-import {Blog} from "./pages/blog";
 
 const App: React.FC = () => {
   return (
@@ -18,12 +12,6 @@ const App: React.FC = () => {
       <Router>
         <Header />
         <Route path="/" exact component={Home} />
-        <Route path="/about" exact component={About} />
-        <Route path="/projects" exact component={Projects} />
-        <Route path="/team" exact component={Team} />
-        <Route path="/contact" exact component={Contact} />
-        <Route path="/partners" exact component={Partners} />
-        <Route path="/blog" exact component={Blog} />
         <Footer />
       </Router>
     </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,19 +7,10 @@ export class Header extends React.Component<any, any> {
   public render() {
     return <header>
       <Navbar expand="lg">
-        <Link to="/">
-          <img src="/images/logo.png" width="265" alt="Serbian Case For Space Foundation" />
-        </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
             <Link className="nav-link" to="/">Home</Link>
-            <Link className="nav-link" to="/about">About</Link>
-            <Link className="nav-link" to="/projects">Projects</Link>
-            <Link className="nav-link" to="/blog">Blog</Link>
-            <Link className="nav-link" to="/team">Team</Link>
-            <Link className="nav-link" to="/partners">Partners</Link>
-            <Link className="nav-link" to="/contact">Contact</Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
