8f7c064a5f4ebf929d09e5da818e4a1181d77f27
Ivan Stanojevic <ivanstan@gmail.com>
2020-09-10 17:05:14 +0200

    add smart cities

--- src/pages/Hackaton.tsx
+++ src/pages/Hackaton.tsx
@@ -27,7 +27,8 @@ class Hackaton extends React.Component<any, any> {
                 <article>
                   <h1 className={'h4'}>Climate change</h1>
                   <div className={'d-md-flex'}>
-                    <img src={'/images/hackathon/thermometer.svg'} alt={'Climate change'} className={'d-block mx-auto my-4 mr-md-4'} style={{maxWidth: 200}}/>
+                    <img src={'/images/hackathon/thermometer.svg'} alt={'Climate change'}
+                         className={'d-block mx-auto my-4 mr-md-4'} style={{ maxWidth: 200 }}/>
                     <p>
                       Changes in the global climate pose a threat to our societies, economies and way of living
                       affecting
@@ -49,7 +50,8 @@ class Hackaton extends React.Component<any, any> {
                 <article>
                   <h1 className={'h4'}>Agriculture</h1>
                   <div className={'d-md-flex'}>
-                    <img src={'/images/hackathon/plant.svg'} alt={'Agriculture'} className={'d-block mx-auto my-4 mr-md-4'} style={{maxWidth: 200}}/>
+                    <img src={'/images/hackathon/plant.svg'} alt={'Agriculture'}
+                         className={'d-block mx-auto my-4 mr-md-4'} style={{ maxWidth: 200 }}/>
                     <p>
                       Farmers around the world have been using Earth Observation and other remote sensing data, such as
                       Sentinel, to plan, monitor and analyze their fields, crops and yield. Planet data brings along a
@@ -65,6 +67,24 @@ class Hackaton extends React.Component<any, any> {
                       governments to understand the effects of the pandemic on the local agricultural industries
                       providing
                       creative solutions for field monitoring.
+                    </p>
+                  </div>
+                </article>
+                <article>
+                  <h1 className={'h4'}>Smart Cities</h1>
+                  <div className={'d-md-flex'}>
+                    <img src={'/images/hackathon/city.svg'} alt={'Agriculture'}
+                         className={'d-block mx-auto my-4 mr-md-4'} style={{ maxWidth: 200 }}/>
+                    <p>
+                      According to the European Commission, to live in a Smart City is to live in a city where modern
+                      technologies are used to improve the quality of life of its inhabitants. Managing city resources,
+                      sustainable urban development, smart urban mobility, policy and regulation monitoring, open data
+                      governance, integrated planning and management, among others, are some of the aspects of urban
+                      life
+                      that can be tackled using modern day tools, data and services. Using Planet’s imagery, you’ll have
+                      access to near-daily monitoring capabilities to design integrated smart city solutions that can
+                      help
+                      solve current limitations on the aforementioned topics.
                     </p>
                   </div>
                 </article>
